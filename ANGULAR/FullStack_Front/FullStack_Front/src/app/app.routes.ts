acf3a79e838a3a2e3fb5916b7dbbd9dd9ba9a60f
loverkity121 <loverkity121@gmail.com>
2025-07-21 17:55:57 -0500

    se creo un not found

--- ANGULAR/FullStack_Front/FullStack_Front/src/app/app.routes.ts
+++ ANGULAR/FullStack_Front/FullStack_Front/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { EstudianteListComponent } from './Component/estudiante-list.component/estudiante-list.component';
 import { EstudianteFormComponent } from './Component/estudiante-form-component/estudiante-form-component';
+import { NotFoundComponent } from './Component/not-found-component/not-found-component';
 
 export const routes: Routes = [
     {path:"" , component:EstudianteListComponent},
-    {path:"formularioEstudiante" , component:EstudianteFormComponent}
+    {path:"formularioEstudiante" , component:EstudianteFormComponent},
+      { path: '**', component: NotFoundComponent }, // ruta comodín (404)
+
 ];
